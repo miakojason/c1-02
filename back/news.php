8006990f5d060c1c5fc850954b4346cd2ef9022e
miakojason <miakojason@gmail.com>
2024-03-02 00:09:09 +0800

    admin管理者帳號ok 先睡囉12步驟差選單明天加油打完再加一遍拚兩遍

--- back/news.php
+++ back/news.php
@@ -8,10 +8,43 @@
                 <td width="7%">刪除</td>
                 <td></td>
             </tr>
-            
+            <?php
+            $total = $DB->count();
+            $div = 5;
+            $pages = ceil($total / $div);
+            $now = $_GET['p'] ?? 1;
+            $start = ($now - 1) * $div;
+            $rows = $DB->all(" limit $start,$div");
+            foreach ($rows as $row) {
+            ?>
+                <tr>
+                    <td><textarea name="text[]" id="" cols="30" rows="10"><?= $row['text']; ?></textarea></td>
+                    <td><input type="checkbox" name="sh[]" value="<?= $row['id']; ?>" <?= ($row['sh'] == 1) ? 'checked' : ''; ?>></td>
+                    <td>
+                        <input type="hidden" name="id[]" value="<?= $row['id']; ?>">
+                        <input type="checkbox" name="del[]" value="<?= $row['id']; ?>">
+                    </td>
+                    <td><input type="button" value="更新圖片" onclick="op('#cover','#cvr','./model/upload.php?table=<?= $do; ?>&id=<?= $row['id']; ?>')"></td>
+                </tr>
+            <?php
+            }
+            ?>
         </table>
         <div class="cent">
-        
+            <?php
+            if ($now > 1) {
+                $prev = $now - 1;
+                echo "<a href='?do=$do&p=$prev'><</a>";
+            }
+            for ($i = 1; $i <= $pages; $i++) {
+                $fontsize = ($now == $i) ? '24px' : '16px';
+                echo "<a href='?do=$do&p=$i'style='font-size:$fontsize'>$i</a>";
+            }
+            if ($now < $pages) {
+                $next = $now + 1;
+                echo "<a href='?do=$do&p=$next'>></a>";
+            }
+            ?>
         </div>
         <table style="margin-top:40px; width:70%;">
             <tr>
---
+++ back/total.php
@@ -0,0 +1,19 @@
+<div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
+	<p class="t cent botli">進佔總人數管理</p>
+	<form method="post" action="./api/bottom.php">
+		<table width="100%">
+			<tr class="yel">
+				<td width="45%">進佔總人數</td>
+				<td width="7%"><input type="text" name="bottom" value="<?=$Total->find(1)['total'];?>"></td>
+			</tr>
+		</table>
+		<table style="margin-top:40px; width:70%;">
+			<tr>
+				<input type="hidden" name="table" value="<?= $do; ?>">
+				<td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置">
+				</td>
+			</tr>
+		</table>
+
+	</form>
+</div>
