01d1a635142fc0a0466c3fae78c90c9b4383c38e
miakojason <miakojason@gmail.com>
2024-03-01 13:56:26 +0800

    login+news切好

--- back.php
+++ back.php
@@ -19,7 +19,7 @@
 		</div>
 	</div>
 	<div id="main">
-		<a title="" href="?">
+		<a title="" href="./index.php">
 			<div class="ti" style="background:url(&#39;use/&#39;); background-size:cover;"></div><!--標題-->
 		</a>
 		<div id="ms">
--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 		</div>
 	</div>
 	<div id="main">
-		<a title="" href="./home_files/home.htm">
+		<a title="" href="./index.php">
 			<div class="ti" style="background:url(&#39;use/&#39;); background-size:cover;"></div><!--標題-->
 		</a>
 		<div id="ms">
@@ -61,7 +61,7 @@
 			</script>
 			<div class="di di ad" style="height:540px; width:23%; padding:0px; margin-left:22px; float:left; ">
 				<!--右邊-->
-				<button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;" onclick="lo(&#39;?do=admin&#39;)">管理登入</button>
+				<button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;" onclick="location.href='?do=login'">管理登入</button>
 				<div style="width:89%; height:480px;" class="dbor">
 					<span class="t botli">校園映象區</span>
 					<script>
