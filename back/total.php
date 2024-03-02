79547304d27b8bccb1ecae22f8a88c9dca9b5b30
miakojason <miakojason@gmail.com>
2024-03-02 23:55:24 +0800

    新增頁尾管理頁面以及add info api 修改人數total因為name是bottom導致寫入失敗變為0，有即時發現

--- back/total.php
+++ back/total.php
@@ -1,10 +1,10 @@
 <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
 	<p class="t cent botli">進佔總人數管理</p>
-	<form method="post" action="./api/bottom.php">
+	<form method="post" action="./api/edit_info.php">
 		<table width="100%">
 			<tr class="yel">
 				<td width="45%">進佔總人數</td>
-				<td width="7%"><input type="text" name="bottom" value="<?=$Total->find(1)['total'];?>"></td>
+				<td width="7%"><input type="text" name="total" value="<?=$Total->find(1)['total'];?>"></td>
 			</tr>
 		</table>
 		<table style="margin-top:40px; width:70%;">
