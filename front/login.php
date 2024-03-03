883a0468a73e1c07b1140a9b8fe2c421efd39bc8
miakojason <miakojason@gmail.com>
2024-03-03 21:36:56 +0800

    精神不太好news不太熟練 明天希望可以一遍半

--- front/login.php
+++ front/login.php
@@ -8,7 +8,13 @@ if (isset($_GET['error'])) {
 ?>
 <div class="di" style="height:540px; border:#999 1px solid; width:53.2%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
 	<marquee scrolldelay="120" direction="left" style="position:absolute; width:100%; height:40px;">
-	</marquee>
+	<?php
+		$ads = $Ad->all(['sh' => 1]);
+		foreach ($ads as $ad) {
+			echo $ad['text'] . " / ";
+		}
+		?>
+</marquee>
 	<div style="height:32px; display:block;"></div>
 	<!--正中央-->
 	<form method="post" action="./api/check.php">
--- front/news.php
+++ front/news.php
@@ -1,10 +1,61 @@
 <div class="di" style="height:540px; border:#999 1px solid; width:53.2%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
 	<marquee scrolldelay="120" direction="left" style="position:absolute; width:100%; height:40px;">
+		<?php
+		$ads = $Ad->all(['sh' => 1]);
+		foreach ($ads as $ad) {
+			echo $ad['text'] . " / ";
+		}
+		?>
 	</marquee>
-	<div style="height:32px; display:block;"></div>
-	<!--正中央-->
-	<div style="text-align:center;">
-		<a class="bl" style="font-size:30px;" href="?do=meg&p=0">&lt;&nbsp;</a>
-		<a class="bl" style="font-size:30px;" href="?do=meg&p=0">&nbsp;&gt;</a>
-	</div>
+	<h3>更多最新消息顯示區</h3>
+	<hr>
+
+	<?php
+	$total = $News->count(['sh' => 1]);
+	$div = 5;
+	$pages = ceil($total / $div);
+	$now = $_GET['p'] ?? 1;
+	$start = ($now - 1) * $div;
+	$news = $News->all(['sh' => 1], " limit $start,$div");
+	?>
+	<ol start="<?= $start + 1; ?>">
+		<?php
+		foreach ($news as $new) {
+		?>
+			<li class="sswww">
+				<?= mb_substr($new['text'], 0, 20); ?>...
+				<div class="all" style="display:none"><?= $new['text']; ?></div>
+			</li>
+		<?php
+		}
+		?>
+		<?php
+		if ($now > 1) {
+			$prev = $now - 1;
+			echo "<a href='?do=$do&p=$prev'><</a>";
+		}
+		for ($i = 1; $i <= $pages; $i++) {
+			$fontsize = ($now == $i) ? '24px' : '16px';
+			echo "<a href='?do=$do&p=$i'style='font-size:$fontsize'>$i</a>";
+		}
+		if ($now < $pages) {
+			$next = $now + 1;
+			echo "<a href='?do=$do&p=$next'>></a>";
+		}
+		?>
 </div>
+<script>
+	$(".sswww").hover(
+		function() {
+			$("#alt").html("" + $(this).children(".all").html() + "").css({
+				"top": $(this).offset().top - 50
+			})
+			$("#alt").show()
+		}
+	)
+	$(".sswww").mouseout(
+		function() {
+			$("#alt").hide()
+		}
+	)
+</script>
