15b75ee7c1d3cdc71e12418b5d6bbb3bd72b2583
miakojason <miakojason@gmail.com>
2024-03-03 11:23:04 +0800

    完成前台倫波顯示，準備上課先到這

--- front/main.php
+++ front/main.php
@@ -3,33 +3,44 @@
 		<?php
 		$ads = $Ad->all(['sh' => 1]);
 		foreach ($ads as $ad) {
-		echo $ad['text'] . " / ";
+			echo $ad['text'] . " / ";
 		}
 		?>
 	</marquee>
 	<div style="height:32px; display:block;"></div>
+
+	<div style="width:100%; padding:2px; height:290px;">
+		<div id="mwww" loop="true" style="width:100%; height:100%;">
+			<div style="width:99%; height:100%; position:relative;" class="cent">沒有資料</div>
+		</div>
+	</div>
 	<!--正中央-->
 	<script>
 		var lin = new Array();
+		<?php
+		$lins = $Mvim->all(['sh' => 1]);
+		foreach ($lins as $lin) {
+			// echo "lin.push('{$lin['img']}');";
+		?>
+			lin.push('<?= $lin['img']; ?>'); //往上19行陣列塞進去所有圖
+		<?php
+		}
+		?>
 		var now = 0;
+		ww(); //1先執行一次 像預設值設定
 		if (lin.length > 1) {
 			setInterval("ww()", 3000);
 			now = 1;
 		}
 
 		function ww() {
-			$("#mwww").html("<embed loop=true src='" + lin[now] + "' style='width:99%; height:100%;'></embed>")
+			$("#mwww").html("<embed loop=true src='./img/" + lin[now] + "' style='width:99%; height:100%;'></embed>")
 			//$("#mwww").attr("src",lin[now])
 			now++;
 			if (now >= lin.length)
 				now = 0;
 		}
 	</script>
-	<div style="width:100%; padding:2px; height:290px;">
-		<div id="mwww" loop="true" style="width:100%; height:100%;">
-			<div style="width:99%; height:100%; position:relative;" class="cent">沒有資料</div>
-		</div>
-	</div>
 	<div style="width:95%; padding:2px; height:190px; margin-top:10px; padding:5px 10px 5px 10px; border:#0C3 dashed 3px; position:relative;">
 		<span class="t botli">最新消息區
 		</span>
